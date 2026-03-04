f87a0d6e56ad8ec317fb3d5c837b5d270e3b1021
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-03-04 08:25:45 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,21 +7,13 @@
   import Loader from "$lib/components/ui/Loader.svelte";
   import "../styles/globals.css";
 
-  let authState
-let uiState
-
-const unsubAuth = auth.subscribe(v => authState = v)
-const unsubUI = ui.subscribe(v => uiState = v)
-  const unsubAuth = auth.subscribe(v => $auth = v);
-  const unsubUI = ui.subscribe(v => $ui = v);
-
   onMount(() => {
     ui.init();
     auth.check();
   });
 </script>
 
-{#if authState?.loading}
+{#if $auth.loading}
 <div class="h-screen flex items-center justify-center bg-[#0b0c10]">
   <Loader text="Loading JSMail..." />
 </div>
